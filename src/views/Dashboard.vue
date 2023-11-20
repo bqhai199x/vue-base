3b8e1a8e40406d1a1e0927aeb758b7b2f735fbda
HaiBQ <haibq@saisystem.vn>
2023-11-20 10:46:46 +0700

    remove temp api

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,13 +3,4 @@
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue';
-import api from 'callers/rest-client';
-import dialog from 'utilities/dialog';
-
-const cate = ref(null);
-onMounted(async () => {
-  cate.value = await api.get('category');
-  await dialog.showConfirm('TT', cate.value);
-});
 </script>
